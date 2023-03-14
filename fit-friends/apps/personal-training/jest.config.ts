91c9d6ff45044eeedc819ff113518696c2f57537
Khariton90 <82729640+Khariton90@users.noreply.github.com>
2023-03-14 18:05:24 +0300

    Создаст стартовые заготовки для микросервисов, добавит схему проекта

---
+++ fit-friends/apps/personal-training/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'personal-training',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/personal-training',
+};
